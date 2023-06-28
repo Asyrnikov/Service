2d39b12b206392584a9e4782403992f70c73b27f
Asyrnikov <71493974+Asyrnikov@users.noreply.github.com>
2023-06-28 22:21:56 +0300

    first commit

---
+++ backend/database/seeders/AddRoleAndPermissionSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create(['name' => 'create-jobs']);
+        Permission::create(['name' => 'edit-orders']);
+        Permission::create(['name' => 'show-all-orders']);
+
+        Permission::create(['name' => 'create-orders']);
+        Permission::create(['name' => 'show-this-user-orders']);
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $adminRole->givePermissionTo([
+            'create-jobs',
+            'edit-orders',
+            'show-all-orders',
+        ]);
+
+        $userRole->givePermissionTo([
+            'create-orders',
+            'show-this-user-orders',
+        ]);
+    }
+}
